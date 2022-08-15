6d31976cc390ff482c4b15b41d58e86e305bbf58
Anton Jaldegren <anton.jaldegren@gmail.com>
2022-08-15 10:25:54 +0200

    Changed Hello to Bye

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+	res.send("Bye World!");
 });
 
 app.listen(PORT, () => {
-  console.log(`haha yes, the server is running on port ${PORT}`);
+	console.log(`haha yes, the server is running on port ${PORT}`);
 });
