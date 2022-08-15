5f486dd411ca6a2e6fac8f424b6b250947f6e07b
Anton Jaldegren <anton.jaldegren@gmail.com>
2022-08-15 10:29:25 +0200

    Changed Bye to Good morning

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send("Bye World!");
+	res.send("Good morning World!");
 });
 
 app.listen(PORT, () => {
